cd32eca18255235285c7dc03b9f3306880f80b2c
HuaTsai <huatsai42@gmail.com>
2026-07-18 22:22:08 +0800

    feat: add ReduceSumV1 (sequential index, removes warp divergence)

--- include/reduce.hpp
+++ include/reduce.hpp
@@ -2,16 +2,16 @@
 
 cudaError_t ReduceSumV0(const float *d_in, float *d_out, int n);
 
-// cudaError_t ReduceSumV1(const float *d_in, float *d_out, int n);
-//
+cudaError_t ReduceSumV1(const float *d_in, float *d_out, int n);
+
 // cudaError_t ReduceSumV2(const float *d_in, float *d_out, int n);
-//
+
 // cudaError_t ReduceSumV3(const float *d_in, float *d_out, int n);
-//
+
 // cudaError_t ReduceSumV4(const float *d_in, float *d_out, int n);
-//
+
 // cudaError_t ReduceSumV5(const float *d_in, float *d_out, int n);
-//
+
 // cudaError_t ReduceSumV6(const float *d_in, float *d_out, int n);
-//
+
 // cudaError_t ReduceSumV7(const float *d_in, float *d_out, int n);
--- tests/test_reduce.cc
+++ tests/test_reduce.cc
@@ -17,6 +17,17 @@ TEST(ReduceSumV0, SimpleSum) {
   ASSERT_EQ(res, n);
 }
 
+TEST(ReduceSumV1, SimpleSum) {
+  constexpr int n = 1000000;
+  std::vector<float> h_in(n, 1.f);
+  DeviceBuffer<float> d_in(n), d_out(1);
+  d_in.from_host(h_in.data());
+  CudaCheck(ReduceSumV1(d_in.get(), d_out.get(), n));
+  float res = 0;
+  d_out.to_host(&res);
+  ASSERT_EQ(res, n);
+}
+
 TEST(ReduceSumV0, ThrowWithInvalidN) {
   DeviceBuffer<float> d_in(1), d_out(1);
   EXPECT_EQ(ReduceSumV0(d_in.get(), d_out.get(), 0), cudaErrorInvalidValue);
